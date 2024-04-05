d8ed314718556e4c44c46cbf66a61d0b77d25b06
Niv Ozer <niv.ozer@yahoo.com>
2024-04-05 22:15:05 +0300

    neatter styling file

--- src/components/Anime.tsx
+++ src/components/Anime.tsx
@@ -6,10 +6,10 @@ function Anime() {
       <div className="AnimeContainer">
         <div className="episodeNumPlay">
           <input type="text" placeholder="Episode Number" />
-          <button className="playEpisode">Play Episode</button>
+          <button className="playEpisode animeButton">Play Episode</button>
         </div>
         <div className="nextEpisode">
-          <button>Next Episode</button>
+          <button className="animeButton">Next Episode</button>
         </div>
       </div>
       <p>Last Episode Watched:</p>
