3aa0bcca0a525e517720d53894e48733ec144e9e
Niv Ozer <niv.ozer@yahoo.com>
2024-04-12 23:37:27 +0300

    Home changes

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,6 +3,7 @@ import Banner from "./Banner";
 import onePunchManLogo from "../banners/one-punch-man.jpg";
 import onePieceLogo from "../banners/one-piece.png";
 import attackOnTitanLogo from "../banners/attack-on-titan.jpg";
+
 function Home() {
   return (
     <div>
