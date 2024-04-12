aa258a4d7a5d07ee489b6a9e65cae33d60040718
Niv Ozer <niv.ozer@yahoo.com>
2024-04-12 23:28:01 +0300

    Works via import

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,16 +1,12 @@
 import React from "react";
-
-function Banner({ imagePath }: { imagePath: string }) {
-  const imgSrc = `./banners/${imagePath}`;
-  console.log(imgSrc);
+interface Props {
+  bannerImage: string;
+}
+function Banner({ bannerImage }: Props) {
   return (
     <div>
       <h2>Text</h2>
-      <img
-        src={
-          "https://cdn.oneesports.gg/cdn-data/2023/09/Anime_OnePiece_MonkeyDLuffy_Wallpaper_Smiling_HD.jpg"
-        }
-      />
+      <img src={bannerImage} />
     </div>
   );
 }
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import Banner from "./Banner";
+import onePunchManLogo from "../banners/one-punch-man.jpg";
+import onePieceLogo from "../banners/one-piece.png";
+import attackOnTitanLogo from "../banners/attack-on-titan.jpg";
 function Home() {
   return (
     <div>
-      <Banner imagePath="attack-on-titan.png" />
+      <Banner bannerImage={attackOnTitanLogo} />
     </div>
   );
 }
